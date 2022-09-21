b71843002f6df96c3b899710408a478fb86a188d
Rob Relyea <rob@relyeas.net>
2022-09-21 13:20:26 -0700

    bogle.tools/invest initial commit

---
+++ src/Shared/InvestementPriority.cs
@@ -0,0 +1,18 @@
+    public class InvestementPriority
+    {
+        public string? step { get; set; }
+        public string? title { get; set; }
+        public string? diagramTitle { get; set; }
+        public string? bestDiagramTitle {
+            get {
+                if (diagramTitle != null) { return diagramTitle; }
+                else { return title; }
+            }
+        }
+        public int number {get; set;}
+        public string? level {get; set;}
+        public string? summary { get; set; }
+        public string? priority { get; set; }
+        public string? returns { get; set; }
+        public string? description { get; set; }
+    }
