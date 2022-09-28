0f4212df64e0064d5d2f55af6914d5f9bd1c64ce
Rob Relyea <rob@relyeas.net>
2022-09-27 18:10:12 -0700

    restructure employer match

--- src/Shared/EmployerPlan.cs
+++ src/Shared/EmployerPlan.cs
@@ -9,19 +9,45 @@ public class EmployerPlan {
     public bool Eligible { get; set; }
     public bool NotEligible {get {return !Eligible;}}
 
-    public int? PercentMatched { get; set; }
-    public int? MatchLimit { get; set; }
+    public int? AnnualSalary { get; set; }
+    public int? MatchA { get; set; }
+    public int? MatchALimit { get; set; }
+    public int? MatchB { get; set; }
+    public int? MatchBLimit { get; set; }
+    public int? MaxMatch { get; set; }
     public int? AmountToSaveForMatch { 
         get
         {
             double? contribution = null;
-            if (PercentMatched != null && MatchLimit != null && PercentMatched!= 0) {
-                var percent = (PercentMatched ?? 0) / 100.0;
-                var matchLimit = MatchLimit ?? 0;
-                contribution = matchLimit / percent;
+            if (MatchALimit != null) {
+                double percentA = (MatchALimit ?? 100) / 100.0;
+                if (percentA > 0) contribution = percentA * AnnualSalary;
+                if (MatchB != null) {
+                    double percentB = (MatchBLimit ?? 100) / 100.0;
+                    if (percentB > 0) contribution = percentB * AnnualSalary;
+                }
             }
 
             return (int?)contribution;
+        }
+    }
+    public int? MatchAmount { 
+        get
+        {
+            double? match = null;
+            if (MatchA != null) {
+                var percentA = (MatchA ?? 0) / 100.0;
+                var matchALimit = (MatchALimit ?? 100.0) / 100.0;
+                match = percentA * AnnualSalary * matchALimit;
+            }
+            if (MatchB != null) {
+                var percentB = (MatchB ?? 0) / 100.0;
+                var matchALimit = (MatchALimit ?? 100.0) / 100.0;
+                var matchBLimit = (MatchBLimit ?? 100.0) / 100.0;
+                match += percentB * AnnualSalary * (matchBLimit - matchALimit);
+            }
+
+            return (int?)match;
         }
     }
     public bool CompleteMatched { get { return AmountToSaveForMatch != null; } }
