5b93df555dda3035ff5b6f5126b2633d76cd8cba
Rob Relyea <rob@relyeas.net>
2023-01-20 14:15:33 -0800

    move files to root from \src

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using src;
 using System.Text.Json;
+using src;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
