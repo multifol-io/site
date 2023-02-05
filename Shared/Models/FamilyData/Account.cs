78b7addc115c5b0a2df73e6dff8fdc563d6d9abc
Rob Relyea <rob@relyeas.net>
2023-02-05 08:02:35 -0800

    add support for Schwab CSV and better errors for files that aren't known

--- Shared/Models/FamilyData/Account.cs
+++ Shared/Models/FamilyData/Account.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Text.Json.Serialization;
 
 public class Account 
@@ -47,115 +48,158 @@ public class Account
 
     public static List<Account> ImportCSV(string[] lines, IList<Fund> funds)
     {
-        string? lastAccountNumber = null;
         List<Account> importedAccounts = new();
 
-        //Fidelity Line 1: Account Number,Account Name,Symbol,Description,Quantity,Last Price,Last Price Change,Current Value
-        //Vanguard Line 1: Account Number,Investment Name,Symbol,Shares,Share Price,Total Value,
-        //Schwab Line 1: "Positions for CUSTACCS as of 09:13 PM ET, 02/04/2023","","","","","","","","","","","","","","","",""
-        //Schwab Line 2: "","","","","","","","","","","","","","","","",""
-        //Schwab Line 3: "Individual ...###","","","","","","","","","","","","","","","",""
-        //Schwab Line 4: "Symbol","Description","Quantity","Price","Price Change %","Price Change $","Market Value","Day Change %","Day Change $","Cost Basis","Gain/Loss %","Gain/Loss $","Ratings","Reinvest Dividends?","Capital Gains?","% Of Account","Security Type"
-        //Schwab End of Account: "Cash & Cash Investments","--","--","--","--","--","$5.20","0%","$0.00","--","--","--","--","--","--","0.29%","Cash and Money Market"
-        //Schwab Totals of Account: "Account Total","--","--","--","--","--","$51,767.73","-1.77%","-$31.93","$547.79","33.1%","$181.34","--","--","--","--","--"
-
         // Process the header
-        var headerChunks = lines[0].Split(',');
+        int lineIndex = 0;
+        string? line = lines[lineIndex++];
+        var headerChunks = line.Split(',');
 
-        int accountNumberIndex = 0; // shouldn't hardcode, but for some strange reason it wasn't matching for Fidelity CSV files!?!
-        int accountNameIndex = -1;
-        int symbolIndex = -1;
-        int investmentNameIndex = -1;
-        int investmentValueIndex = -1;
-        
-        int chunkIndex = 0;
-        foreach (var headerChunk in headerChunks) {
-            switch (headerChunk) {
-                case "Account Name":
-                    accountNameIndex = chunkIndex;
-                    break;
-                case "Symbol":
-                    symbolIndex = chunkIndex;
-                    break;
-                case "Investment Name":
-                case "Description":
-                    investmentNameIndex = chunkIndex;
-                    break;
-                case "Current Value":
-                case "Total Value":
-                case "Market Value":
-                    investmentValueIndex = chunkIndex;
-                    break;
-                case "Account Number":
-                    accountNumberIndex = chunkIndex;
-                    break;
-                default:
-                    break;
-            }
-
-            chunkIndex++;
-        }
-
-        Account? currentAccount = null;
-        for (int lineNum = 1;lineNum < lines.Length; lineNum++) {
-            var line = lines[lineNum];
-            string? accountNumber = null;
-            string? accountName = null;
-            string? symbol = null;
-            string? investmentName = null;
-            string? investmentValue = null;
-            if (line.Length > 0)
+        if (headerChunks.Length > 2) {
+            if (headerChunks[0].StartsWith("Account Number") && headerChunks[1] == "Account Name")
             {
-                if (char.IsDigit(line[0]) || char.IsDigit(line[1]))
+                // FIDELITY - blank line ends investment listings.
+                // Line 0: Account Number,Account Name,Symbol,Description,Quantity,Last Price,Last Price Change,Current Value
+                string lastAccountNumber = "";
+                line = lines[lineIndex++];
+                Account? newAccount = null;
+                while (line != "")
                 {
-                    // Process this data
                     var chunks = line.Split(',');
-                    
-                    for (int i=0; i < chunks.Length; i++) {
-                        if (accountNumberIndex == i) {
-                            accountNumber = chunks[i];
-                        } else if (accountNameIndex == i) {
-                            accountName = chunks[i];
-                        } else if (symbolIndex == i) {
-                            symbol = chunks[i];
-                        } else if (investmentNameIndex == i) {
-                            investmentName = chunks[i];
-                        } else if (investmentValueIndex == i) {
-                            investmentValue = chunks[i];
-                        }
-                    }
+                    var accountNumber = chunks[0];
+                    var accountName = chunks[1];
+                    var symbol = chunks[2];
+                    var investmentName = chunks[3];
 
-                    if (lastAccountNumber != accountNumber) {
-                        currentAccount = new () { 
-                            Note = accountName != null ? "⚠️" + accountName: "⚠️*"+ accountNumber.Substring(accountNumber.Length-4,4),
-                            Custodian = (accountNameIndex != -1 ? "Fidelity" : "Vanguard")
-                        };
-                        importedAccounts.Add(currentAccount);
-                        lastAccountNumber = accountNumber;
-                    }
-                    
+                    var investmentValue = chunks[7];
                     if (investmentValue[0] == '$')
                     {
                         investmentValue = investmentValue.Substring(1);
                     }
+
                     double doubleValue = 0.0;
                     double.TryParse(investmentValue, out doubleValue);
+                    if (lastAccountNumber != accountNumber)
+                    {
+                        newAccount = new() {
+                            Custodian = "Fidelity",
+                            Note = "⚠️*"+ accountNumber.Substring(accountNumber.Length-4,4) + " " + accountName
+                        };
+                        importedAccounts.Add(newAccount);
+                        lastAccountNumber = accountNumber;
+                    }
 
+                    Investment newInvestment = new () { funds = funds, Ticker = symbol, Name = investmentName, Value = doubleValue };
+                    newAccount?.Investments.Add(newInvestment);
+                    line = lines[lineIndex++];
+                }
+            } 
+            else if (headerChunks[0] == "Account Number" && headerChunks[1] == "Investment Name")
+            {
+                // VANGUARD - blank lines seperates accounts. two blank lines end investment listing.
+                // Line 0: Account Number,Investment Name,Symbol,Shares,Share Price,Total Value,
+                int lastAccountStartLineIndex = -1;
+                line = null;
+                Account? newAccount = null;
+                while (lastAccountStartLineIndex != lineIndex - 1) {
+                    lastAccountStartLineIndex = lineIndex;
+                    if (line == null || line == "") {
+                        newAccount = null;
+                        line = lines[lineIndex++];
+                        while (line != "")
+                        {
+                            var chunks = line.Split(',');
+                            var accountNumber = chunks[0];
+                            var symbol = chunks[2];
+                            var investmentName = chunks[1];
+                            var investmentValue = chunks[5];
+                            if (investmentValue[0] == '$')
+                            {
+                                investmentValue = investmentValue.Substring(1);
+                            }
 
-                    if (symbol != null || investmentName != null) {
-                        Investment newInvestment = new () { funds = funds, Ticker = symbol, Name = investmentName, Value = doubleValue };
-                        currentAccount.Investments.Add(newInvestment);
+                            double doubleValue = 0.0;
+                            double.TryParse(investmentValue, out doubleValue);
+                            if (newAccount == null) {
+                                newAccount = new() {
+                                    Custodian = "Vanguard",
+                                    Note = "⚠️*"+ accountNumber.Substring(accountNumber.Length-4,4)
+                                };
+                                importedAccounts.Add(newAccount);
+                            }
+
+                            Investment newInvestment = new () { funds = funds, Ticker = symbol, Name = investmentName, Value = doubleValue };
+                            newAccount?.Investments.Add(newInvestment);
+                            line = lines[lineIndex++];
+                        }
                     }
                 }
-
-                if (line.Length > 1 && char.IsLetter(line[1])) // some account numbers start with a letter
+            }
+            else if (headerChunks[0].StartsWith("\"Positions for "))
+            {
+                // SCHWAB - line with blank cells divides accounts
+                // Line 0: "Positions for CUSTACCS as of 09:13 PM ET, 02/04/2023","","","","","","","","","","","","","","","",""
+                // Line 1: "","","","","","","","","","","","","","","","",""
+                // Line 2: "Individual ...###","","","","","","","","","","","","","","","",""
+                // Line 3: "Symbol","Description","Quantity","Price","Price Change %","Price Change $","Market Value","Day Change %","Day Change $","Cost Basis","Gain/Loss %","Gain/Loss $","Ratings","Reinvest Dividends?","Capital Gains?","% Of Account","Security Type"
+                // 0-n lines: investment info
+                // End of Account: "Cash & Cash Investments","--","--","--","--","--","$5.20","0%","$0.00","--","--","--","--","--","--","0.29%","Cash and Money Market"
+                // Totals of Account: "Account Total","--","--","--","--","--","$51,767.73","-1.77%","-$31.93","$547.79","33.1%","$181.34","--","--","--","--","--"
+                var chunks = lines[lineIndex++].Split("\",\"");
+                Account? newAccount = null;
+                while (chunks.Length > 0 && chunks[0] == "\"")
                 {
-                    // stop processing file at first line that starts with a letter.
-                    break;
+                    var accountNameChunks = lines[lineIndex++].Split(',');
+                    newAccount = new() {
+                        Custodian = "Schwab",
+                        Note = "⚠️"+ accountNameChunks[0].Substring(1, accountNameChunks[0].Length - 2)
+                    };
+
+                    if (newAccount != null) {  // TODO: only doing to prove to compiler that ctor won't throw ... best way?
+                        importedAccounts.Add(newAccount);
+                        lineIndex++; // skip investmentHeaders line
+                        chunks = lines[lineIndex++].Split("\",\"");
+                        while (lineIndex < lines.Length - 1 && chunks[0] != "\"") {
+                            var symbol = chunks[0].Substring(1); // front quote wasn't removed by split.
+                            string? investmentName = null;
+                            string? investmentValue;
+                            switch (symbol) {
+                                case "Cash & Cash Investments":
+                                    symbol = "CASH";
+                                    investmentValue = chunks[6];
+                                    break;
+                                case "Account Total":
+                                    investmentValue = null;
+                                    break;
+                                default:
+                                    investmentName = chunks[1];
+                                    investmentValue = chunks[6];
+                                    break;
+                            }
+
+                            if (investmentValue != null) {
+                                double doubleValue = 0.0;
+                                double.TryParse(investmentValue,
+                                                        NumberStyles.AllowCurrencySymbol | NumberStyles.Float | NumberStyles.AllowThousands,
+                                                        CultureInfo.GetCultureInfoByIetfLanguageTag("en-US"),
+                                                        out doubleValue);
+                                Investment newInvestment = new () { funds = funds, Ticker = symbol, Name = (investmentName != null ? investmentName : null) , Value = doubleValue };
+                                newAccount?.Investments.Add(newInvestment);
+                            }
+
+                            chunks = lines[lineIndex++].Split("\",\"");
+                        }
+                    }
                 }
             }
+            else
+            {
+                throw new InvalidDataException("CSV file doesn't appear to be Fidelity, Schwab, or Vanguard");
+            }
+        } else {
+            throw new InvalidDataException("CSV file doesn't appear to be Fidelity, Schwab, or Vanguard");
         }
-    
+
         return importedAccounts;
     }
 }
