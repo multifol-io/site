1c758e13ab99fd46ef62c31bf54e25a492f68d77
Rob Relyea <rob@relyeas.net>
2023-03-22 13:49:00 -0700

    strip ** from end of fidelity tickers

--- Shared/Models/FamilyData/Importer.cs
+++ Shared/Models/FamilyData/Importer.cs
@@ -167,6 +167,12 @@ public class Importer {
                     var accountNumber = chunks[0];
                     var accountName = chunks[1];
                     var symbol = chunks[2];
+
+                    // many tickers have "**" at end, signifying money market fund.
+                    if (symbol.Length >= 2 && symbol.Substring(symbol.Length-2) == "**") { 
+                        symbol = symbol.Substring(0, symbol.Length - 2);
+                    }
+
                     string? investmentName;
                     double value;
                     double? shares = null;
