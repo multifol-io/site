3d673c468c9ca0e35060cb93f82ec6f6cde31f64
Rob Relyea <rob@relyeas.net>
2023-07-07 20:32:21 -0700

    support adding and editing grants

--- Shared/Models/FamilyData/RSUGrant.cs
+++ Shared/Models/FamilyData/RSUGrant.cs
@@ -1,11 +1,12 @@
 public class RSUGrant {
     public DateTime Date { get; set; }
     public string? Ticker { get; set; }
-    public int VestEventsCount { get; set; }
+    public int? VestEventsCount { get; set; }
     public int VestPeriodMonths { get; set;}
     public double Amount { get; set; }
     public double? Price { get; set; }
     public int? Shares { get; set; }
+    public bool Edit { get; set; }
 
     private double? _LastPrice;
     public double? LastPrice { 
