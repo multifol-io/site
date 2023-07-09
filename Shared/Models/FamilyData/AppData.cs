81c5d8f3e48282ecd13f4b819350b0e5ddaecc7f
Rob Relyea <rob@relyeas.net>
2023-07-08 21:55:28 -0700

    portfolio tracker: views via links. fixes 129

--- Shared/Models/FamilyData/AppData.cs
+++ Shared/Models/FamilyData/AppData.cs
@@ -1,4 +1,5 @@
 using IRS;
+using System.Text.Json.Serialization;
 
 public class AppData : IAppData
 {
@@ -13,6 +14,7 @@ public class AppData : IAppData
     public IRSData IRSData {get; set;}
     public string EODHistoricalDataApiKey { get; set;}
     public int Year {get; set;} 
+    [JsonIgnore] // no longer using as of 7/8/2023, stop saving.
     public string PortfolioView { get; set; }
     public string RSUView { get; set; }
     public ImportResult? ImportResult { get; set; }
