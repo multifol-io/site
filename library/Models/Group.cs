a01ab145e54bba0d4b734d3f4d8c539e6bcab42e
Rob Relyea <rob@relyeas.net>
2024-02-23 10:07:06 -0800

    Group<T,U> to support grouping of data items

---
+++ library/Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using DocumentFormat.OpenXml.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Group<T,U> : List<U> where U : class
+{
+    public T GroupInfo { get; private set; }
+
+    public Group(T groupInfo, List<U> items) : base(items)
+    {
+        GroupInfo = groupInfo;
+    }
+}
