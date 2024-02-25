2c5cac52c488483d29a306ef937535c516ce2bb0
Rob Relyea <rob@relyeas.net>
2024-02-25 15:54:19 -0800

    Group<T,U>.Create

--- library/Models/Group.cs
+++ library/Models/Group.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-public class Group<T,U> : List<U> where U : class
+public class Group<T, U> : List<U> where U : class
 {
     public T GroupInfo { get; private set; }
 
@@ -14,4 +14,24 @@ public class Group<T,U> : List<U> where U : class
     {
         GroupInfo = groupInfo;
     }
+
+    public static List<Group<T, U>> Create(IEnumerable<T> collection, string itemsPropertyName)
+    {
+        var groups = new List<Group<T, U>>();
+        var propInfo = typeof(T).GetProperty(itemsPropertyName);
+        foreach (var outerObject in collection)
+        {
+            var group = new Group<T, U>(outerObject, (List<U>)propInfo.GetValue(outerObject));
+            groups.Add(group);
+        }
+
+        return groups;
+    }
 }
+
+public class HoldingGroupInfo
+{
+    public string InvestmentOrderCategory { get; set; }
+    public double? Value { get; set; }
+}
+
