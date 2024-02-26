696e03f38e3252e0b4832b10e7722fdca9e1b3e9
Rob Relyea <rob@relyeas.net>
2024-02-26 06:52:02 -0800

    try rFINIA name. back buttons.

--- library/Models/FamilyData/FamilyData.cs
+++ library/Models/FamilyData/FamilyData.cs
@@ -299,7 +299,7 @@ public class FamilyData
             var overallTotal = (StockBalance ?? 0.0) + (InternationalStockBalance ?? 0.0) + (BondBalance ?? 0.0) + (OtherBalance ?? 0.0) + (CashBalance ?? 0.0);
             if (overallTotal > 0.0)
             {
-                return ( (StockBalance ?? 0.0) + (InternationalStockBalance ?? 0.0) ) / overallTotal;
+                return ( (StockBalance ?? 0.0) + (InternationalStockBalance ?? 0.0) ) / overallTotal * 100.0;
             }
             else
             {
@@ -313,7 +313,7 @@ public class FamilyData
             var stockTotal = (StockBalance ?? 0.0) + (InternationalStockBalance ?? 0.0);
             if (stockTotal > 0.0)
             {
-                return (InternationalStockBalance ?? 0.0) / stockTotal;
+                return (InternationalStockBalance ?? 0.0) / stockTotal * 100.0;
             }
             else
             {
@@ -332,7 +332,7 @@ public class FamilyData
 
             if (overallTotal > 0.0)
             {
-                return (BondBalance ?? 0.0) / overallTotal;
+                return (BondBalance ?? 0.0) / overallTotal * 100.0;
             }
             else
             {
